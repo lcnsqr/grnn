83477eb95d915db322ade2ec0d55c116c3287d64
Luciano Siqueira <lcnsqr@gmail.com>
2018-07-26 01:48:10 -0300

    Opções para escolher os parâmetros de criação dos conjuntos para a equação do calor

--- README.md
+++ README.md
@@ -19,12 +19,22 @@ Localização das bibliotecas Cuda (linha 37): `CUDA_PATH ?= "/usr/local/cuda"`
 
 Sem nenhum argumento, os comandos `grnn_{cpu,pthreads,gpu}` apenas estimam o erro das estimativas para o conjunto de  teste. Os conjuntos de treinamento e teste são gerados pelo comando `geradorDifusao` ou `geradorMandelbrot`. São criados os arquivos `train.bin` e `test.bin`, que serão utilizados pelo comando `grnn_{cpu,pthreads,gpu}` para computar as estimativas.
 
-Um arquivo para armazenar as estimativas pode ser informado com a opção `-o result.bin` para que as estimativas sejam armazenadas no arquivo `result.bin`.
-
-Um escalar para o parâmetro sigma da regressão pode ser informado com a opção `-s ESCALAR`, onde `ESCALAR` é um valor real positivo. O valor padrão é 1.
-
-## Conjunto Mandelbrot
+### Conjunto Mandelbrot
 
 Além dos resultados para a equação do calor, podem ser estimados os pontos do conjunto fractal Mandelbrot. Os comandos `geradorMandelbrot` e `verMandelbrot` são compilados separadamente com o comando `make -f Makefile.mandelbrot`. São necessárias as bibliotecas de desenvolvimento **SDL2** para compilar esses comandos.
 
 O comando `geradorMalndelbrot` abre um tela com a representação gráfica do conjunto Mandelbrot. A tecla **z** aproxima, a tecla **x** afasta e as teclas de direção transladam o semiplano. A tecla **s** armazena em *train.bin* o conjunto de treinamento para o semiplano exibido e a tecla **t** armazena em *test.bin* o conjunto de teste para o semiplano exibido. Depois de gerar um arquivo de resultados com o comando `grnn_{cpu,omp,pthreads,gpu} result.bin`, a imagem com as estimativas para o conjunto de teste é exibida com o comando `verMaldelbrot result.bin`.
+
+### Equação do Calor
+
+O comando `geradorDifusao` tem três opções: `-t`, `-e` e `-d`. A opção `-t TOTAL` define em `TOTAL` a quantidade de amostras de treinamento e a opção `-t TOTALTEST` define em `TOTALTEST` a quantidade de amostras de teste, onde `TOTAL` e `TOTALTEST` são números inteiros maiores que zero.
+
+A opção `-d` define a quantidade de pontos igualmente espaçados no intervalo da solução, o que também determina o número de dimensões das variáveis independentes e dependentes da regressão.
+
+### Gerador das estimativas 
+
+Os comandos geradores das estimativas `grnn_{cpu,pthreads,gpu}` têm duas opções: `-o` e `-s`.
+
+Um arquivo para armazenar as estimativas pode ser informado com a opção `-o result.bin` para que as estimativas sejam armazenadas no arquivo `result.bin`.
+
+Um escalar para o parâmetro sigma da regressão pode ser informado com a opção `-s ESCALAR`, onde `ESCALAR` é um valor real positivo. O valor padrão é 1.
--- geradorDifusao.c
+++ geradorDifusao.c
@@ -24,15 +24,6 @@
 #define MINVAL 0
 #define MAXVAL 1
 
-// Total de amostras
-#define TOTAL 20480000
-
-// Total de amostras de teste
-#define TOTALTEST 100
-
-// Dimensões da amostra
-#define DIM 6
-
 // Arquivo das amostras
 #define TRAIN "train.bin"
 // Arquivo das amostras de teste
@@ -43,40 +34,63 @@
 
 // Função de preenchimento da condição inicial.
 void inicial(Matrix *w){
+	// Pontos na curva inicial
+	int dim = w->rows - 1;
 	// Gerar uma curva aleatória
 	w->_[0] = RAND * MAXVAL;
-	for (int i = 1; i < DIM; i++){
+	for (int i = 1; i < dim; i++){
 		w->_[i] = w->_[i-1] - .25 + .5 * RAND;
 		if ( w->_[i] < MINVAL ) w->_[i] = MINVAL;
 		if ( w->_[i] > MAXVAL ) w->_[i] = MAXVAL;
 	}
 	// Valor da derivada em relação a x no extremo 
 	// b definido pelo último valor do vetor (coluna)
-	w->_[DIM] = 0;
+	w->_[dim] = 0;
 }
 
 int main (int argc, char **argv){
+	// Opções da linha de comando
+	int total = 20480000;
+	int totaltest = 1024;
+	int dim = 6;
+	for(int i = 1; i < argc; i++){
+		switch (argv[i][1]){
+		case 't':
+			// Total de amostras
+			total = atoi(argv[i+1]);
+		break;
+		case 'e':
+			// Total de amostras de teste
+			totaltest = atoi(argv[i+1]);
+		break;
+		case 'd':
+			// Dimensões da amostra
+			dim = atoi(argv[i+1]);
+		break;
+		}
+	}
+
+	// Semente aleatória
 	srand((unsigned int)time(NULL));
 
 	// Conjunto de treinamento
 	struct pathSet train;
 	// Usar float (4 bytes)
 	train.type = 0x0f;
-	train.total = TOTAL;
+	train.total = total;
 	train.vertices = 2;
 	train.dim = (unsigned int*)malloc(2*sizeof(unsigned int));
-	train.dim[0] = DIM;
-	train.dim[1] = DIM;
+	train.dim[0] = dim;
+	train.dim[1] = dim;
 	// Tamanho de um caminho (soma das dimensões dos vértices)
-	unsigned int sdim = train.dim[0]+train.dim[1];
-	train.size = train.total*sdim*sizeof(float);
+	train.size = train.total*(train.dim[0]+train.dim[1])*sizeof(float);
 	train.data.f = (float*)malloc(train.size);
 	
 	// Vetores antes e depois (condição inicial e vetor resultante)
 	Matrix w[2];
 	// Alocar e incluir dimensão para a condição de Neuman
-	mtrxBuild(&w[0], DIM + 1, 1);
-	mtrxBuild(&w[1], DIM + 1, 1);
+	mtrxBuild(&w[0], dim + 1, 1);
+	mtrxBuild(&w[1], dim + 1, 1);
 	// Gerar cada amostra e copiar para o conjunto de treinamento
 	printf("Gerando %d amostras de treinamento...\n", train.total);
 	for (int i = 0; i < train.total; i++){
@@ -85,12 +99,10 @@ int main (int argc, char **argv){
 		// Gerar as soluções por diferenças finitas, após T segundos
 		gerar(w, ALFA, POS0, POS1, T);
 		// Copiar v.i. para posição correspondente na memória do conjunto
-		//memcpy(&train.data.f[i*sdim], w[0]._, train.dim[0]*sizeof(float));
 		for (int j = 0; j < train.dim[0]; j++){
 			train.data.f[i + j * train.total] = w[0]._[j];
 		}
 		// Variável dependente
-		//memcpy(&train.data.f[i*sdim+train.dim[0]], w[1]._, train.dim[1]*sizeof(float));
 		for (int j = 0; j < train.dim[1]; j++){
 			train.data.f[train.total*train.dim[0] + i + j * train.total] = w[1]._[j];
 		}
@@ -106,12 +118,12 @@ int main (int argc, char **argv){
 	struct pathSet test;
 	// Usar float (4 bytes)
 	test.type = 0x0f;
-	test.total = TOTALTEST;
+	test.total = totaltest;
 	test.vertices = 2;
 	test.dim = (unsigned int*)malloc(2*sizeof(unsigned int));
-	test.dim[0] = DIM;
-	test.dim[1] = DIM;
-	test.size = test.total*sdim*sizeof(float);
+	test.dim[0] = dim;
+	test.dim[1] = dim;
+	test.size = test.total*(train.dim[0]+train.dim[1])*sizeof(float);
 	test.data.f = (float*)malloc(test.size);
 	
 	// Gerar cada amostra e copiar para o conjunto de teste
@@ -122,17 +134,13 @@ int main (int argc, char **argv){
 		// Gerar as soluções por diferenças finitas, após T segundos
 		gerar(w, ALFA, POS0, POS1, T);
 		// Copiar v.i. para posição correspondente na memória do conjunto
-		//memcpy(&test.data.f[i*sdim], w[0]._, test.dim[0]*sizeof(float));
 		for (int j = 0; j < test.dim[0]; j++){
 			test.data.f[i + j * test.total] = w[0]._[j];
 		}
 		// Variável dependente
-		//memcpy(&test.data.f[i*sdim+test.dim[0]], w[1]._, test.dim[1]*sizeof(float));
 		for (int j = 0; j < test.dim[1]; j++){
 			test.data.f[test.total*test.dim[0] + i + j * test.total] = w[1]._[j];
 		}
-		// Somente o último componente
-		//test.data.f[i*sdim+test.dim[0]] = w[1]._[DIM-1];
 		// Exibir progresso
 		if ( (i+1) % 10000 == 0 ) printf("%d\n", i+1);
 	}
