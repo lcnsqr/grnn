d50b6956889201d4dd54fd5b38728ce1b345eaf8
Luciano Siqueira <lcnsqr@gmail.com>
2018-07-25 22:11:17 -0300

    Alteração da disposição dos dados nos conjuntos de treinamento e teste para o conjunto Mandelbrot

--- verMandelbrot.c
+++ verMandelbrot.c
@@ -53,9 +53,12 @@ void render(struct Context *ctx){
 	#pragma omp parallel for private(x,y,c,rgba)
 	for (int i = 0; i < ctx->view.frameSize; i += ctx->view.depth){
 		// Conjunto do resultado
-		x = ctx->result.data.f[3*(i/ctx->view.depth)];
-		y = ctx->result.data.f[3*(i/ctx->view.depth)+1];
-		c = floor(ctx->iterations * ctx->result.data.f[3*(i/ctx->view.depth)+2]);
+		//x = ctx->result.data.f[3*(i/ctx->view.depth)];
+		//y = ctx->result.data.f[3*(i/ctx->view.depth)+1];
+		//c = floor(ctx->iterations * ctx->result.data.f[3*(i/ctx->view.depth)+2]);
+		x = ctx->result.data.f[ (i/ctx->view.depth) ];
+		y = ctx->result.data.f[ (i/ctx->view.depth) + WIDTH*HEIGHT ];
+		c = floor(ctx->iterations * ctx->result.data.f[ (i/ctx->view.depth) + WIDTH*HEIGHT*2 ]);
 		// Pixels
 		corMap(&ctx->view.cor, c, rgba);
 		ctx->view.frame[i] = (char)rgba[0];
